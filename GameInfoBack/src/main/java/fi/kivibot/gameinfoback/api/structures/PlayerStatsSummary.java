8074adad7705ff6a8e98d8bf48268ddb9486825d
Nicklas Ahlskog <kivi@kivibot.fi>
2015-02-14 18:04:31 +0200

    Added the new classes

---
+++ GameInfoBack/src/main/java/fi/kivibot/gameinfoback/api/structures/PlayerStatsSummary.java
@@ -0,0 +1,37 @@
+package fi.kivibot.gameinfoback.api.structures;
+
+/**
+ *
+ * @author Nicklas
+ */
+public class PlayerStatsSummary {
+    
+    private final int losses;
+    private final long modifyDate;
+    private final String playerStatSummaryType;
+    private final int wins;
+
+    public PlayerStatsSummary(int losses, long modifyDate, String playerStatSummaryType, int wins) {
+        this.losses = losses;
+        this.modifyDate = modifyDate;
+        this.playerStatSummaryType = playerStatSummaryType;
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public long getModifyDate() {
+        return modifyDate;
+    }
+
+    public String getPlayerStatSummaryType() {
+        return playerStatSummaryType;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+    
+}
